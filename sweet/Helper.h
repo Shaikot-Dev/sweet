b565a0a17cc3874b162a273c1a9b9965d271dfce
Anisur Rahman Shaikot <52798642+Shaikot-Dev@users.noreply.github.com>
2020-02-25 21:35:35 +0600

    complete with default time (30s)

--- sweet/Helper.h
+++ sweet/Helper.h
@@ -1,78 +1,85 @@
-#ifdef HELPER_H
+#ifndef HELPER_H
+
 #define HELPER_H
 
 #include <ctime>
 #include <string>
 #include <sstream>
+#include <fstream>
 
-namespace Helper{
+namespace Helper // custom namespace to store our custom data types
+{
+	template <class T>
 
-    template <class T>
-    std: :string ToString(const T &);
-    struct DateTime{
-        DateTime(){
-            time_t ms;
-            time (&ms);
+	std::string ToString(const T &);
 
-            struct tm*info = localtime(&ms)
-            D = info->tm_mday;
-            m = info->tm_mon + 1;
-            y = 1900 + info->tm_year;
-            M = info->tm_min;
-            H = info->tm_hour;
-            s = info->tm_sec;
+	struct DateTime
+	{
+		int D, m, y, M, H, S;
 
-        }
-        DateTime(int D, int m, int y, int H,int M, int S) : D(D), m(m),Y(Y), H(H), M(M), S(S) {}
-        DateTime(int D, int m, int y) : D(D), m(m),Y(Y), H(H), M(M), S(S) {}
+		DateTime()
+		{
+			time_t ms;
+			time(&ms);
 
-        DateTime Now() Const{
-            return DateTime();
+			struct tm* info = localtime(&ms);
 
-        }
-            int D, m, Y, H, M, S;
+			// format data from info
 
-            std: :string GetDateString() const{
-                return std: :string(D < 10 ? "0" : "") + ToString(D) +
-                        std: :string(m < 10 ? ".0" : ".") + ToString(m) + "."+
-                        ToString(y);
-        }
+			D = info->tm_mday;
+			m = info->tm_mon + 1; // need to add 1 since january is represented 0
+			y = 1900 + info->tm_year; // reference year since C came in the 70s and locatime returns time from that particular day until present day
+			M = info->tm_min;
+			H = info->tm_hour;
+			S = info->tm_sec;
+		}
 
-        std: :string GetTimeString(const std: :string &sep = ":") const{
-            return std: :string (H < 10 ? "0" : "") + ToString(H) + sep +
-                    std: :string (M < 10 ? "0" : "") + ToString(H) + sep +
-                    std: :string (S < 10 ? sep : "") + ToString(S)
+		DateTime(int D, int m, int y, int M, int H, int S) : D(D), m(m), y(y), M(M), H(H), S(S) {}
+		DateTime(int D, int m, int y) : D(D), m(m), y(y), M(0), H(0), S(0) {}
 
-        }
+		DateTime Now() const
+		{
+			return DateTime(); // return current date time
+		}
 
-        std: :string GetDateTimeString(const std: :string &sep = ":") const{
+		std::string GetDateString() const
+		{
+			// Generate the current date that is correctly formatted in string
+			return std::string(D < 10 ? "0" : "") + ToString(D) +
+				std::string(m < 10 ? ".0" : ".") + ToString(m) + "." + ToString(y);
+		}
 
-            return GetDateString() + " " + GetTimeString(sep;)
-        }
+		std::string GetTimeString(const std::string &sep = ":") const// reference is to default separator which is set to a colon
+		{
+			// Generate the current time that is correctly formatted in string
+			return std::string(H < 10 ? "0" : "") + ToString(H) + sep +
+				std::string(M < 10 ? "0" : "") + ToString(M) + sep +
+				std::string(S < 10 ? "0" : "") + ToString(S);
+		}
 
+		std::string GetDateTimeString(const std::string &sep = ":") const
+		{
+			return GetDateString() + " " + GetTimeString(sep);
+		}
+	};
 
+	template <class T>
 
+	std::string ToString(const T &e) // only able types that supports the insertion operator
+	{
+		std::ostringstream s;
+		s << e;
+		return s.str();
+	}
 
-    };
-        template <class T>
-        std: :string ToString(const T &e){
-            std: :ostringstream s;
-            s << e;
-            return s.str();
+	// OPTIONAL FUNCTION FOR DEBUGGING PURPOSES
 
-    }
-    void WriteAppLog (const std: :string &s){
-        std: :ofstream file("Applog.txt", std: :ios: :app );
-        file <<  "[" << Helper: :DateTime().GetDateTimeString() << "]" <<
-        "\n" << s << std: :string << "\n";
-        file.close();
-
-    }
-
-
+	void WriteAppLog(const std::string &s) // reference to const string we wish to log
+	{
+		std::ofstream file("AppLog.txt", std::ios::app); // app stands for append file
+		file << "[" << Helper::DateTime().GetDateTimeString() << "]" << "\n" << s << std::endl << "\n";
+		file.close();
+	}
 }
-
-
-
 
 #endif // HELPER_H
